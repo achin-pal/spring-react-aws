76723bfede9183cc9b54ae80903bb72d860ab6dc
Achin <achin.pal7@gmail.com>
2024-05-22 17:13:28 -0600

    h2 dbadded

---
+++ src/main/java/com/demo/basicApp/ChallangeRepository.java
@@ -0,0 +1,6 @@
+package com.demo.basicApp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallangeRepository extends JpaRepository {
+}
--- src/main/java/com/demo/basicApp/Challanges.java
+++ src/main/java/com/demo/basicApp/Challanges.java
@@ -1,10 +1,18 @@
 package com.demo.basicApp;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Challanges {
+    @Id
     private Long id;
+    @Column(name = "challangeMonth")
     private String month;
     private String description;
 
+    public Challanges(){}
     public Challanges(Long id, String month, String description) {
         this.id = id;
         this.month = month;
